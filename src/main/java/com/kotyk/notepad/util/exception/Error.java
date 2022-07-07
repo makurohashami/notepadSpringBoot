9dbe178424aae7008ab9f35ed3d2149e3a142f8c
Yaroslav Kotyk <makurohashami@gmail.com>
2022-07-07 08:31:53 +0300

    added pattern for time

--- src/main/java/com/kotyk/notepad/util/exception/Error.java
+++ src/main/java/com/kotyk/notepad/util/exception/Error.java
@@ -1,11 +1,14 @@
 package com.kotyk.notepad.util.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 
 import java.util.Date;
 
 @Getter
 public class Error {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     private Date timestamp;
     private String message;
     private String details;
